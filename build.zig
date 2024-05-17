a24df858dffa3c5f7bcdfcc169a374f1dbb9d988
echoptic <bigerchocho@gmail.com>
2024-05-17 14:50:25 +0200

    Update for zig 0.13 master

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .name = "bitflags",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -32,12 +32,12 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    _ = b.addModule("root", .{ .root_source_file = .{ .path = "src/root.zig" } });
+    _ = b.addModule("root", .{ .root_source_file = b.path("src/root.zig") });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
